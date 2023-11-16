5e6e4ba5de6da752e4858191a7441e92789db2dd
Ayrat Khaydarov <info.chelovechek@gmail.com>
2023-11-17 00:25:13 +0300

    Solved problem "1980. Find Unique Binary String" using hash set and brute force of all available unique values.

---
+++ LeetCode.Problems/Hard/Problem1980.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Problems.Hard
+{
+    /// <summary>
+    /// Solution for problem 1980. Find Unique Binary String
+    /// https://leetcode.com/problems/find-unique-binary-string/description/
+    /// </summary>
+    public class Problem1980
+    {
+        public string FindDifferentBinaryString(string[] nums)
+        {
+            HashSet<int> values = nums.Select(n => Convert.ToInt32(n, 2)).ToHashSet();
+
+            string binaryString = string.Empty;
+
+            // 65535 is 1111111111111111 in binary
+            for (var value = 0; value <= 65535; value++)
+            {
+                if (!values.Contains(value))
+                {
+                    binaryString = Convert.ToString(value, 2);
+
+                    break;
+                }
+            }
+
+            return binaryString.PadLeft(nums.Length, '0');
+        }
+    }
+}
---
+++ LeetCode.UnitTests/Hard/Test1980.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode.Problems.Hard;
+using NUnit.Framework;
+
+namespace LeetCode.UnitTests.Hard
+{
+    /// <summary>
+    /// Test for solution of problem 1980. Find Unique Binary String
+    /// https://leetcode.com/problems/find-unique-binary-string/description/
+    /// </summary>
+    [TestFixture]
+    public class Test1980
+    {
+        private Problem1980 _solution; 
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _solution = new Problem1980();
+        }
+
+        [TestCase("01", "10")]
+        [TestCase("00", "01")]
+        [TestCase("111", "011", "001")]
+        public void FindDifferentBinaryStringTest(params string[] nums)
+        {
+            string actual = _solution.FindDifferentBinaryString(nums);
+
+            Assert.That(actual, Is.Not.Null, "Actual value is null!");
+
+            Assert.That(
+                actual.Length,
+                Is.EqualTo(nums.Length),
+                "Actual value's length is not equal to nums length!");
+
+            Assert.That(
+                nums, 
+                Is.Not.Contains(actual), 
+                "Actual value is not unique in nums array!");
+        }
+    }
+}
