9c3240ae625a6da23203f064c27342d7f9813c91
Ayrat Khaydarov <info.chelovechek@gmail.com>
2023-11-18 01:57:20 +0300

    Solved problem "1877. Minimize Maximum Pair Sum in Array" using sorting and pointers from the beginning and end to find the max pair sum.

---
+++ LeetCode.Problems/Hard/Problem1877.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Problems.Hard
+{
+    /// <summary>
+    /// Solution for problem 1877. Minimize Maximum Pair Sum in Array
+    /// https://leetcode.com/problems/minimize-maximum-pair-sum-in-array/description/
+    /// </summary>
+    public class Problem1877
+    {
+        public int MinPairSum(int[] nums)
+        {
+            Array.Sort(nums);
+
+            var maxSum = int.MinValue;
+
+            for (var index = 0; index < nums.Length / 2; index++)
+            {
+                maxSum = Math.Max(
+                    maxSum, 
+                    nums[index] + nums[nums.Length - 1 - index]);
+            }
+
+            return maxSum;
+        }
+    }
+}
---
+++ LeetCode.UnitTests/Hard/Test1877.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode.Problems.Hard;
+using NUnit.Framework;
+
+namespace LeetCode.UnitTests.Hard
+{
+    /// <summary>
+    /// Test for solution of problem 1877. Minimize Maximum Pair Sum in Array
+    /// https://leetcode.com/problems/minimize-maximum-pair-sum-in-array/description/
+    /// </summary>
+    [TestFixture]
+    public class Test1877
+    {
+        private Problem1877 _solution;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _solution = new Problem1877();
+        }
+
+        [TestCase(7, 3, 5, 2, 3)]
+        [TestCase(8, 3, 5, 4, 2, 4, 6)]
+        public void MinPairSumTest(int expected, params int[] nums)
+        {
+            int actual = _solution.MinPairSum(nums);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
